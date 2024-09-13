782411e0f5d373f0b4f20ac5cf9e0493d6e18133
Taeyoon Lee <taeyoon.lee@xa.epicgames.com>
2024-09-13 12:10:41 +0900

    celery base

---
+++ README.md
@@ -0,0 +1,6 @@
+## run backend
+> $ docker run -p 6379:6379 -d redis/redis-stack
+
+## run a celery worker with 4 child processes
+> $ celery -app tasks worker --concurrency 4 --pool threads --loglevel=INFO
+
---
+++ tasks.py
@@ -0,0 +1,36 @@
+from celery import Celery
+import threading
+import time
+from importlib import import_module
+import sys
+import os
+
+sys.path.append('.')
+
+app = Celery('tasks', broker='redis://localhost:6379/0', result_backend='redis://')
+
+@app.task
+def add(x, y):
+    #print(current_task.request.id)
+    #time.sleep(10)
+    s = 0
+    thread_code = int(threading.current_thread().name[-1])
+    for i in range(100):
+        for j in range(100):
+            for k in range(100):
+                for q in range(100):
+                    s += thread_code + 1 + i + j + k
+    print(f'The name of the current thread is {threading.current_thread().name}.')
+    return x + y + s
+
+
+@app.task
+def run_new_code(file_name, task_id):
+    try:
+        os.system('git pull')
+    except:
+        print('git pull error')
+    if file_name in sys.modules:
+        del sys.modules[file_name]
+    new_code = import_module(file_name)
+    return new_code.main(task_id)
